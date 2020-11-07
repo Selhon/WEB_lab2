1e0dd89a8e8631c1d61358d7c68c5ee8b772cfc6
svire <svirel2000@mail.ru>
2020-11-08 02:50:29 +0300

    сделан "краткое содержание"

--- web/animationActivation.js
+++ web/animationActivation.js
@@ -42,22 +42,19 @@ $('.text-button-prog').on('mouseout', function () {
 });
 
 
-
-
 $('.text-button-artist').on('mouseover', function () {
     $('.jump-button-artist')[0].classList.toggle('pass-button-art-z');
     $('.jump-button-artist')[0].classList.toggle('active-button-art-z');
 });
-$('.text-button-artist').on('mouseover', function () {
+$('.text-button-artist').on('mouseover', function (){setTimeout(function () {
     $('.jump-button-artist')[0].classList.toggle('pass-button-art');
     $('.jump-button-artist')[0].classList.toggle('active-button-art');
-});
-
+},250)});
 $('.text-button-artist').on('mouseout', function () {
     $('.jump-button-artist')[0].classList.toggle('pass-button-art');
     $('.jump-button-artist')[0].classList.toggle('active-button-art');
 });
-$('.text-button-artist').on('mouseout', function () {
+$('.text-button-artist').on('mouseout', function (){setTimeout(function () {
     $('.jump-button-artist')[0].classList.toggle('pass-button-art-z');
     $('.jump-button-artist')[0].classList.toggle('active-button-art-z');
-});
+},250)});
